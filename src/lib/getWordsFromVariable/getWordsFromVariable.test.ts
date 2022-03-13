9b913c9abd09d45a0fd5bb83896f336203a3c890
Shinyaigeek <Philispaxil@gmail.com>
2022-03-13 15:30:40 +0900

    Feature: use Case package

--- src/lib/getWordsFromVariable/getWordsFromVariable.test.ts
+++ src/lib/getWordsFromVariable/getWordsFromVariable.test.ts
@@ -2,18 +2,27 @@ import { getWordsFromVariable } from "./mod";
 
 describe("getWordsFromVariable", () => {
   test("handle PascalCase", () => {
-    expect(getWordsFromVariable("PascalCase")).toEqual(["pascal", "case"]);
+    expect(getWordsFromVariable("PascalCase")).toEqual(["Pascal", "Case"]);
   });
 
   test("handle camelCase", () => {
-    expect(getWordsFromVariable("camelCase")).toEqual(["camel", "case"]);
+    expect(getWordsFromVariable("camelCase")).toEqual(["Camel", "Case"]);
   });
 
   test("handle snake_case", () => {
-    expect(getWordsFromVariable("snake_case")).toEqual(["snake", "case"]);
+    expect(getWordsFromVariable("snake_case")).toEqual(["Snake", "Case"]);
   });
 
   test("handle SNAKE_CASE", () => {
-    expect(getWordsFromVariable("SNAKE_CASE")).toEqual(["snake", "case"]);
+    expect(getWordsFromVariable("SNAKE_CASE")).toEqual(["Snake", "Case"]);
   });
+
+  test("handle uppercase word in camelCase", () => {
+    expect(getWordsFromVariable("camelCaseWITHUppercase")).toEqual([
+      "Camel",
+      "Case",
+      "WITH",
+      "Uppercase",
+    ]);
+  })
 });
--- src/lib/getWordsFromVariable/mod.ts
+++ src/lib/getWordsFromVariable/mod.ts
@@ -1,49 +1,8 @@
+import Case from "case";
+
 export const getWordsFromVariable: (variable: string) => string[] = function (
   variable
 ) {
-  let currentWord = "";
-  let previousChar = "";
-  return variable
-    .split("")
-    .reduce((words, char, idx, arr) => {
-      if (char === "_") {
-        words.push(currentWord);
-        currentWord = "";
-        previousChar = "";
-        if (idx === arr.length - 1) {
-          words.push(currentWord);
-        }
-        return words;
-      }
-
-      if (isUppercase(previousChar)) {
-        currentWord += char;
-        previousChar = char;
-        if (idx === arr.length - 1) {
-          words.push(currentWord);
-        }
-        return words;
-      }
-
-      if (isUppercase(char)) {
-        if (currentWord) {
-          words.push(currentWord);
-        }
-        currentWord = char;
-        previousChar = char;
-        return words;
-      }
-
-      previousChar = char;
-      currentWord += char;
-      if (idx === arr.length - 1) {
-        words.push(currentWord);
-      }
-      return words;
-    }, [] as string[])
-    .map((word) => word.toLowerCase());
-};
-
-const isUppercase: (char: string) => boolean = function (char) {
-  return char === char.toUpperCase();
+  const dashCased = Case.header(variable);
+  return dashCased.split("-");
 };
