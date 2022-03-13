cbfde59e48bc5db9366b3e89a49e820c74fb5c2a
Shinyaigeek <Philispaxil@gmail.com>
2022-03-13 16:29:57 +0900

    Feature: check also in class name

--- src/rules/ban-orthographical-variant.ts
+++ src/rules/ban-orthographical-variant.ts
@@ -40,6 +40,48 @@ export const banOrthographicalVariant: TSESLint.RuleModule<
           }
         }
       },
+      Property(node) {
+        const propertyName = node.key;
+        if (propertyName.type === "Identifier") {
+          const variableName = propertyName.name;
+          const dictionaryPath = options[0].dictionaryPath;
+          const dictionary = require(dictionaryPath);
+          const words = getWordsFromVariable(variableName);
+          for (const word of words) {
+            if (dictionary[word]) {
+              console.log("HEY");
+            }
+          }
+          return;
+        }
+        if (propertyName.type === "Literal") {
+          const variableName = propertyName.value?.toString();
+          const dictionaryPath = options[0].dictionaryPath;
+          const dictionary = require(dictionaryPath);
+          const words = getWordsFromVariable(variableName || "");
+          for (const word of words) {
+            if (dictionary[word]) {
+              console.log("HEY");
+            }
+          }
+          return;
+        }
+
+        // * if expression, this is computed props. computed props is out of range
+      },
+      ClassDeclaration(node) {
+        if (node.id) {
+          const className = node.id;
+          const dictionaryPath = options[0].dictionaryPath;
+          const dictionary = require(dictionaryPath);
+          const words = getWordsFromVariable(className.name);
+          for (const word of words) {
+            if (dictionary[word]) {
+              console.log("HEY");
+            }
+          }
+        }
+      },
       FunctionDeclaration(node) {
         if (node.id) {
           const variableName = node.id.name;
